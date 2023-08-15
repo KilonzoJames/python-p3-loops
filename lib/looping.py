9b32932f6f02866b3f506c7d8f1b7aac58f53994
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-15 18:24:58 +0300

    Control Flow: Loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python3
-
+'''This is a sample docstring'''
 def happy_new_year():
-    # code goes here!
-    pass
+    '''Returns'''
+    i=10
+    while i > 0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    '''Returns'''
+    squared_list = [int**2 for int in int_list]
+    return squared_list
+input_list = [2, 3, 5, 7, 10]
+print(square_integers(input_list))
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    '''Returns'''
+    for num in range(1,101):
+        if num % 3==0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 ==0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print (num)
+fizzbuzz()
+
+def reverse_string(string):
+    '''Returns'''
+    reversed_string = ""
+    for char in string:
+        reversed_string=char + reversed_string
+    return reversed_string
+
+print(reverse_string('uppercase'))
